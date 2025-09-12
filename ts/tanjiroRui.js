a3635636cd0587d3eb96d33571f92a7c3eb8a431
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-09-12 19:54:49 +0800

    blood and collision

--- ts/tanjiroRui.js
+++ ts/tanjiroRui.js
@@ -2,23 +2,52 @@ var game = document.getElementById('game');
 var tanjiroContainer = document.getElementById('tanjiroContainer');
 var ruiContainer = document.getElementById('ruiContainer');
 var message = document.getElementById('message');
-var tanjiroX = 1090;
-var tanjiroY = 150;
+var tanjiroX = 1350;
+var tanjiroY = 100;
 document.addEventListener('keydown', function (event) {
-    if (event.key === 'ArrowRight') {
+    if (event.key === 'ArrowRight' && tanjiroX < 1350) {
         tanjiroX += 10;
         tanjiroContainer.style.left = "".concat(tanjiroX, "px");
     }
-    else if (event.key === 'ArrowLeft') {
+    else if (event.key === 'ArrowLeft' && tanjiroX > 0) {
         tanjiroX -= 10;
         tanjiroContainer.style.left = "".concat(tanjiroX, "px");
     }
-    else if (event.key === 'ArrowUp') {
+    else if (event.key === 'ArrowUp' && tanjiroY > 0) {
         tanjiroY -= 10;
         tanjiroContainer.style.top = "".concat(tanjiroY, "px");
     }
-    else if (event.key === 'ArrowDown') {
+    else if (event.key === 'ArrowDown' && tanjiroY < 300) {
         tanjiroY += 10;
         tanjiroContainer.style.top = "".concat(tanjiroY, "px");
     }
 });
+function createBloodThread() {
+    var bloodThreadX = 0;
+    var thread = document.createElement('div');
+    thread.className = 'absolute border-2 border-black rounded-full text-center text-lg';
+    // thread.textContent = '🕸️';
+    thread.style.backgroundImage = 'url(./asset/image/spiderweb.png)';
+    thread.style.backgroundSize = 'cover';
+    thread.style.width = '50px';
+    thread.style.height = '50px';
+    thread.style.left = "-100px";
+    thread.style.top = "".concat(Math.floor(Math.random() * 350), "px");
+    game.appendChild(thread);
+    var webMoveInterval = setInterval(function () {
+        bloodThreadX += 5;
+        thread.style.left = "".concat(bloodThreadX, "px");
+        if (bloodThreadX > 1300) {
+            thread.remove();
+            clearInterval(webMoveInterval);
+        }
+        // Collision detection
+        if (bloodThreadX >= tanjiroX && bloodThreadX <= tanjiroX + 60 && parseInt(thread.style.top.replace('px', '')) >= tanjiroY && parseInt(thread.style.top.replace('px', '')) <= tanjiroY + 60) {
+            alert('Tanjiro has been caught by Rui\'s Blood Thread! Game Over!');
+            bloodThreadX = 1400; // Move the thread out of bounds to stop further checks
+            thread.remove();
+        }
+        ;
+    }, 10);
+}
+setInterval(createBloodThread, 2000);
--- ts/tanjiroRui.ts
+++ ts/tanjiroRui.ts
@@ -3,20 +3,55 @@ const tanjiroContainer = document.getElementById('tanjiroContainer') as HTMLDivE
 const ruiContainer = document.getElementById('ruiContainer') as HTMLDivElement|null;
 const message = document.getElementById('message') as HTMLDivElement|null;
 
-let tanjiroX:number = 1090;
-let tanjiroY:number = 150;
+let tanjiroX:number = 1350;
+let tanjiroY:number = 100;
 document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowRight') {
+    if (event.key === 'ArrowRight' && tanjiroX<1350) {
         tanjiroX += 10;
         tanjiroContainer.style.left = `${tanjiroX}px`;
-    }else if (event.key === 'ArrowLeft') {
+    }else if (event.key === 'ArrowLeft' && tanjiroX>0) {
         tanjiroX -= 10;
         tanjiroContainer.style.left = `${tanjiroX}px`;
-    }else if (event.key === 'ArrowUp') {
+    }else if (event.key === 'ArrowUp' && tanjiroY>0) {
         tanjiroY -= 10;
         tanjiroContainer.style.top = `${tanjiroY}px`;
-    }else if (event.key === 'ArrowDown') {
+    }else if (event.key === 'ArrowDown' && tanjiroY<300) {
         tanjiroY += 10;
         tanjiroContainer.style.top = `${tanjiroY}px`;
     }
 });
+
+
+function createBloodThread():void{
+    let bloodThreadX:number=0;
+    const thread = document.createElement('div');
+    thread.className = 'absolute border-2 border-black rounded-full text-center text-lg';
+    // thread.textContent = '🕸️';
+    thread.style.backgroundImage = 'url(./asset/image/spiderweb.png)';
+    thread.style.backgroundSize = 'cover';
+    thread.style.width = '50px';
+    thread.style.height = '50px';
+    thread.style.left = `-100px`;
+    thread.style.top = `${ Math.floor(Math.random() * 350)}px`;
+    game.appendChild(thread);
+
+    const webMoveInterval = setInterval(() => {
+        bloodThreadX +=5;
+        thread.style.left = `${bloodThreadX}px`;
+        if (bloodThreadX > 1300) {
+          thread.remove();
+          clearInterval(webMoveInterval);
+        }
+
+        // Collision detection
+        if(bloodThreadX >= tanjiroX && bloodThreadX <= tanjiroX + 60 && parseInt(thread.style.top.replace('px',''))>= tanjiroY && parseInt(thread.style.top.replace('px','')) <= tanjiroY + 60){
+            alert('Tanjiro has been caught by Rui\'s Blood Thread! Game Over!');
+            bloodThreadX = 1400; // Move the thread out of bounds to stop further checks
+            thread.remove();
+        };
+    }, 10);
+
+
+}
+
+setInterval(createBloodThread, 2000);
