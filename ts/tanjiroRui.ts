ed342c49f6b76c78369ff4b3f41e222ec4974a38
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-09-12 18:04:07 +0800

    setup page

---
+++ ts/tanjiroRui.ts
@@ -0,0 +1,4 @@
+const game = document.getElementById('game') as HTMLDivElement|null;
+const chicken = document.getElementById('chicken') as HTMLDivElement|null;
+const message = document.getElementById('message') as HTMLDivElement|null;
+
